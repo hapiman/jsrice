9af3d40af743965ddf6f8081c24087ac0f7d2e8a
彭建 <jian.peng@fengjr.com>
2018-03-21 20:00:57 +0800

    add method

---
+++ proxy/README.md
@@ -0,0 +1,1 @@
+####  参考文档 [https://cnodejs.org/topic/5a2ffe00d92f2f5b185acde5#5a30a02ed92f2f5b185acdfb](https://cnodejs.org/topic/5a2ffe00d92f2f5b185acde5#5a30a02ed92f2f5b185acdfb)
--- treasure/tools.js
+++ treasure/tools.js
@@ -1,15 +1,18 @@
+'use strict'
 
-//判断是否是对象, 可能为对象或者数组 
+const _ = require('lodash')
+
+//判断是否是对象, 可能为对象或者数组
 var isObject = function(obj) {
   return obj !== null && typeof obj === 'object'
 }
 
-//判断是否是空对象 
+//判断是否是空对象
 var isEmptyObject  = function(obj){
    if(obj === null) return false
    if(typeof  obj != 'object') return false
    if(obj instanceof Array) return false
-   if(Object.keys(obj).length > 0) return false 
+   if(Object.keys(obj).length > 0) return false
    return true
 }
 
@@ -33,17 +36,29 @@ var deepCopy = function(p){
             c[k] = p[k]
         }
     }
-    return c 
+    return c
 }
 
-//获取对象的私有属性 使用hasOwnProperty, for-in会列出全部属性 
-//使用 isPrototypeOf判断是否是选型上的方法 
+//获取对象的私有属性 使用hasOwnProperty, for-in会列出全部属性
+//使用 isPrototypeOf判断是否是选型上的方法
 var printOwnKeys = function(obj){
     for(var k in obj){
         if(obj.hasOwnProperty(k)){
             console.log(k);
         }
     }
+}
+
+//判断是否是’纯’对象，即字典类型
+var isDictionary = function isDictionary(thing) {
+    return _.isObject(thing) && !_.isArray(thing) && !_.isFunction(thing);
+}
+
+// 获取文件的后缀名
+var fileExtension = function(str) {
+    if(str === null) return '';
+    var pieces = String(str).split('.');
+    return pieces.length > 1 ? _.last(pieces) : '';
 }
 
 module.exports = {
@@ -53,5 +68,8 @@ module.exports = {
     extendCopy,
     deepCopy,
 
-    printOwnKeys
+    printOwnKeys,
+    isDictionary,
+    fileExtension
 }
+
