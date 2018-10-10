00146867500c8e6d8be5f3242a38c0007ea8f74e
彭建 <jian.peng@fengjr.com>
2018-10-10 21:55:12 +0800

    Add eslint config

---
+++ eslint/README.md
@@ -0,0 +1,1 @@
+`eslingt`继承了`standard`的用法, 修改`eslintrc`文件, 可以更改`standard`中的规范
--- eslint/index.js
+++ eslint/index.js
@@ -1,10 +1,19 @@
-console.log('122')
-var obj = {
-  www : 'dd',
-}
-var a = 1
-if( 2===a){
-  console.log(1)
-}else {
 
+const sayLove = function (params) {
+  console.log('Say love, ', params)
 }
+
+function sayHello (dd) {
+  console.log('message =>', message)
+  let [
+    a,
+    b
+  ] = [1, 2]
+  console.log('1 =>', a)
+  console.log('1 =>', b)
+}
+
+// sayLove()
+sayHello()
+
+// sayHello('hello lint')
